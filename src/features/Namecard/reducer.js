d78c6b3d5a43147e1ec22cb276ada104cb8cf5c6
Ken Lee <cb3lee@uwaterloo.ca>
2018-03-01 14:01:53 -0500

    state structure group by privacy

--- src/features/Namecard/reducer.js
+++ src/features/Namecard/reducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import { createStatus } from 'Utils/reducer';
+import _ from 'lodash';
 import * as actions from './actions';
 const {
   NAMECARD_LOAD_REQUEST,
@@ -13,14 +14,33 @@ const status = createStatus({
   error: [NAMECARD_LOAD_ERROR]
 });
 
-const entitiesInitial = {};
+const namecards = (state = {}, action) => {
+  switch (action.type) {
+    case NAMECARD_LOAD_SUCCESS: {
+      return _.reduce(
+        action.data,
+        (acc, v) => {
+          if (!acc[v.privacy]) {
+            acc[v.privacy] = [];
+          }
+          acc[v.privacy] = [...acc[v.privacy], v];
+          return acc;
+        },
+        state
+      );
+    }
+    default:
+      return state;
+  }
+};
 
+const entitiesInitial = {};
 const entities = (state = entitiesInitial, action) => {
   switch (action.type) {
     case NAMECARD_LOAD_SUCCESS:
       state = {
         ...state,
-        [action.username]: action.data
+        [action.username]: namecards(state[action.username], action)
       };
       return state;
     default:
