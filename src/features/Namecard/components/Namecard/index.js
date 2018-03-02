544faf11ac6b1dbfd0e247def18a5ec45efb73b7
Beidouz <beidouz@hotmail.com>
2018-03-02 03:13:56 -0500

    reskin namecard

--- src/features/Namecard/components/Namecard/index.js
+++ src/features/Namecard/components/Namecard/index.js
@@ -7,11 +7,13 @@ const Namecard = ({ tag, services, aliases }) => {
   return (
     <div className={styles.root}>
       <div className={styles.tag}>{tag}</div>
-      <div className={styles.aliases}>
-        {aliases && <Aliases content={aliases} />}
-      </div>
-      <div className={styles.services}>
-        {services && <Services content={services} />}
+      <div className={styles.card}>
+        <div className={styles.aliases}>
+          {aliases && <Aliases content={aliases} />}
+        </div>
+        <div className={styles.services}>
+          {services && <Services content={services} />}
+        </div>
       </div>
     </div>
   );
