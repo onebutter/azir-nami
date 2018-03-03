e9238371b7b54d065209092a3a0eb87c48a484b7
Ken <cb3lee@uwaterloo.ca>
2018-03-03 15:37:35 -0500

    gst q ; ;

--- src/containers/Loading/index.js
+++ src/containers/Loading/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import styles from './styles.css';
+import bars from './bars.svg';
 
 class Loading extends React.Component {
   render() {
-    return <div className={styles.root}>loading forever...........</div>;
+    return (
+      <div className={styles.root}>
+        <img src={bars} />
+      </div>
+    );
   }
 }
 
