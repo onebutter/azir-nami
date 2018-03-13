5550c405f450bda771e2fbfaa31ee37904678123
Ken <cb3lee@uwaterloo.ca>
2018-03-12 23:44:54 -0400

    deploy script

---
+++ scripts/deploy.prod.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+if [ ! -f ./package.json ]; then
+    echo "package.json is not found"
+fi
+
+rm -rf build
+rm -rf node_modules
+npm install
+npm run build
+cp ./favicons/* ./build/
+aws s3 sync ./build s3://reachaf/
+
